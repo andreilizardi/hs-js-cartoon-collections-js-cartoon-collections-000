7a7a10432ddef58b8f1b3198a261e931a9f930c5
Andrei Lizardi <andrei.lizardi@gmail.com>
2020-04-08 21:20:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
-function dwarfRollCall(dwarves) {
-}
+// Problema 1
 
-function summonCaptainPlanet(planeteerCalls){
-}
+let dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
 
-function longPlaneteerCalls(words) {
-}
+const dwarfRollCall = (inputArray) => {
+let output = '';
+  dwarfArray.forEach((name, i) => {
+    output += `${i+1}. ${name} `;
+  });
+  return output;
+};
 
-function findTheCheese (foods) {
-}
+// Problema 2
+
+let planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+
+const summonCaptainPlanet = (inputArray) => {
+for(let i = 0; i < inputArray.length; i++) {
+       inputArray[i] = inputArray[i].toUpperCase() + '!';
+  }
+  return inputArray;
+};
+
+// Problema 3
+
+let shortWords = ["wind", "fire"];
+let assortedWords = ["earth", "wind", "heart", "fire"];
+
+const longPlaneteerCalls = (inputArray) => {
+  let output = false;
+
+inputArray.forEach(name => {
+  if (name.length > 4) {
+  output = true;
+  }
+})
+return output;
+};
+
+// Problema 4
+
+const cheeses = ["cheddar", "gouda", "camembert"];
+
+const snacks = ["crackers", "gouda", "thyme"];
+
+const soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"];
+
+const ingredients = ["garlic", "rosemary", "bread"];
+
+const findTheCheese = (inputArray) => {
+let output = `no cheese!`;
+ 
+   for (let i = 0; i < inputArray.length; i++) {
+    let name = inputArray[i];
+    if (cheeses.includes(name)) {
+  output = name;
+          break;
+  }
+   }  
+   return output;
+};
